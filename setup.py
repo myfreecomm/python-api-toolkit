cd12e1076ff799d9f4306cc296b32574a94a5325
Gerardo Nobre Soares <gerardonsoares@gmail.com>
2013-07-24 19:06:47 -0300

    Conserta setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from distutils.core import setup
 
 setup(name='api_toolkit',
       version='0.1',
-      packages=['requests', 'vcrpy'],
+      py_modules=['api_toolkit'],
+      install_requires=[
+          'requests>=1.2.3'
+      ],
+      tests_require=['vcrpy==0.0.3'],
      )
