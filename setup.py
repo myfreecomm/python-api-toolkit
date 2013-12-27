3aa24aa0478b1805beecf3f54c385fc3b371f977
Vitor M. A. da Cruz <vitor.mazzi@myfreecomm.com>
2013-12-27 20:04:19 -0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='api_toolkit',
-      version='0.1',
+      version='0.2',
       packages=['api_toolkit'],
       install_requires=[
           'requests>=1.2.3'
